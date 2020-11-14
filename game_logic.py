3ca2c3007299b156d5a19c5429171d89a868f42b
Rafaello <rafjar@Rafalek>
2020-11-14 18:56:25 +0100

    Init commit

---
+++ game_logic.py
@@ -0,0 +1,57 @@
+from board import Board
+
+
+from snake import Snake
+from board import Board
+from food import Food
+
+
+class Game_logic:
+    def __init__(self):
+        '''
+        Cała logika gry!
+        Ważne jest zliczanie punktów oraz liczby wykonanych przez węża ruchów
+        '''
+        self.points = 0
+        self.move_count = 0
+        self.board = Board()
+        self.snake = Snake(self.board)
+        self.apple = Food(self.snake, self.board)
+        self.print_game()
+        self.snake.move_snake()
+        self.apple.randomize_position()
+        self.print_game()
+
+    def print_game(self):
+        '''
+        Narysowanie gry na ekraniku
+        # - ściana
+        @ - ciało węża
+        O - głowa węża
+        $ - jedzonko
+        '''
+        print(f'Points: {self.points}')
+        print(f'Moves: {self.move_count}')
+        print('#' * (self.board.xsize+2))
+        for row in range(self.board.ysize):
+            print('#', end='')
+            for col in range(self.board.xsize):
+                if (col, row) in self.snake.position:
+                    if self.snake.position.index((col, row)) == 0:
+                        print('O', end='')
+                    else:
+                        print('@', end='')
+                elif col == self.apple.xpos and row == self.apple.ypos:
+                    print('$', end='')
+                else:
+                    print(' ', end='')
+            print('#')
+
+        # print('#' * (self.board.xsize+2)) # odkomenotwać kiedyś
+
+        # To tylko do ogarniania numerów kolumn
+        # później odkomentować to na górze
+        print('#', end='')
+        for i in range(self.board.xsize):
+            print(i % 10, end='')
+        print('#')
---
+++ main.py
@@ -0,0 +1,10 @@
+from game_logic import Game_logic
+from game_logic import *
+
+
+def main():
+    game_logic = Game_logic()
+
+
+if __name__ == '__main__':
+    main()
